7f2b15921bd3783d2c077c3a9af66bc831bbeda8
LKelley212 <laura.kelley@jpschools.org>
2025-09-16 14:48:23 +0000

    platformer updates to complexity

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,25 +27,27 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
     // TODO 2 - Create Platforms
     // createPlatform(X, Y, legnth, height, "color")
 
     createPlatform(300, 635, 350, 20, "black");
     createPlatform(100, 500, 250, 20, "red");
+    createPlatform(230, 420, 50, 20, "purple");
     createPlatform(800, 310, 500, 20, "limegreen");
-    createPlatform(1000, 200, 400, 20, "orange");
-    createPlatform(500, 425, 300, 20, "blue");
+    createPlatform(1000, 200, 225, 20, "orange");
+    createPlatform(1300, 150, 100, 20, "yellow");
+    createPlatform(500, 398, 300, 20, "blue");
     createPlatform(350, 200, 300, 20, "hotpink");
     // TODO 3 - Create Collectables
     // createCollectable("Type", "X, Y, gravity, bounce")
-    createCollectable("heartGem", 1250, 220, 0.5, 0.5);
-    createCollectable("heartGem", 1200, 220, 0.5, 0.5);
-    createCollectable("roundGem", 505, 150, 0.7, 0.5);
-    createCollectable("starGem", 190, 450, 0.5, 0.5);
-    createCollectable("starGem", 215, 450, 0.5, 0.5);
-
+    createCollectable("heartGem", 135, 450, 0.5, 0.5);
+    createCollectable("roundGem", 233, 300, 0.5, 0.5);
+    createCollectable("starGem", 505, 150, 0.7, 0.5);
+    createCollectable("heartGem", 850, 220, 0.5, 0.5);
+    createCollectable("starGem", 1200, 220, 0.5, 0.5);
+    createCollectable("database", 1350, 100);
     // TODO 4 - Create Cannons
     createCannon("right", 700, 1500);
     createCannon("bottom", 1200, 1000);
