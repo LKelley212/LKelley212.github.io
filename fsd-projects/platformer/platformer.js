74a140095300ca513dce8ff1ad5907fcabfcb2d3
LKelley212 <laura.kelley@jpschools.org>
2025-09-22 15:53:18 +0000

    Finalized Platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -33,17 +33,17 @@ $(function () {
     // createPlatform(X, Y, legnth, height, "color")
 
     createPlatform(300, 635, 350, 20, "black");
-    createPlatform(100, 500, 250, 20, "red");
-    createPlatform(230, 420, 50, 20, "purple");
+    createPlatform(75, 500, 250, 20, "red");
+    createPlatform(225, 420, 75, 20, "purple");
     createPlatform(800, 310, 500, 20, "limegreen");
-    createPlatform(1000, 200, 225, 20, "orange");
+    createPlatform(1000, 200, 225, 20, "orange", 950, 1100, 1);
     createPlatform(1300, 150, 100, 20, "yellow");
     createPlatform(500, 398, 300, 20, "blue");
     createPlatform(350, 200, 300, 20, "hotpink");
     // TODO 3 - Create Collectables
     // createCollectable("Type", "X, Y, gravity, bounce")
     createCollectable("heartGem", 135, 450, 0.5, 0.5);
-    createCollectable("roundGem", 233, 300, 0.5, 0.5);
+    createCollectable("roundGem", 240, 300, 0.5, 0.5);
     createCollectable("starGem", 505, 150, 0.7, 0.5);
     createCollectable("heartGem", 850, 220, 0.5, 0.5);
     createCollectable("starGem", 1200, 220, 0.5, 0.5);
