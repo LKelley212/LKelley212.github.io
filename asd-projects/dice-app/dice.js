e5c9be52eb3efab3ec778d3cc1c85336b88cec51
LKelley212 <laura.kelley@jpschools.org>
2025-10-21 17:37:05 +0000

    Dice App updates

--- asd-projects/dice-app/dice.js
+++ asd-projects/dice-app/dice.js
@@ -1,19 +1,25 @@
 $(document).ready(function () {
   // Your code goes here
   $("<div>")
-    .css("height", 15)
-    .css("width", 15)
-    .css("background-color", "black")
+    .css("height", 25)
+    .css("width", 25)
     .css("position", "absolute")
     .css("top", 50)
     .css("left", 50)
     .appendTo("#die");
 
   function makeDot(top, left, elementID) {
+    var colorById = {
+      "#die": "rgb(99, 101, 224)",
+      "#die2": "rgba(255, 255, 255, 1)",
+    };
+    var dotColor = colorById[elementID];
+
     $("<div>")
-      .css("height", 15)
-      .css("width", 15)
-      .css("background-color", "black")
+      .css("height", 18)
+      .css("width", 18)
+      .css("border-radius", "50%") // makes the square a circle
+      .css("background-color", dotColor)
       .css("position", "absolute")
       .css("top", top)
       .css("left", left)
@@ -26,6 +32,13 @@ $(document).ready(function () {
   makeDot(25, 75, "#die"); // top right
   makeDot(50, 75, "#die"); // middle right
   makeDot(75, 75, "#die"); // bottom right
+
+  makeDot(25, 25, "#die2"); // top left
+  makeDot(50, 25, "#die2"); // middle left
+  makeDot(75, 25, "#die2"); // bottom left
+  makeDot(25, 75, "#die2"); // top right
+  makeDot(50, 75, "#die2"); // middle right
+  makeDot(75, 75, "#die2"); // bottom right
 
   function rollDie(dieID) {
     $(dieID).empty();
@@ -64,7 +77,9 @@ $(document).ready(function () {
   }
   function handleClick() {
     rollDie("#die");
+    rollDie("#die2");
   }
   handleClick();
   $("#die").on("click", handleClick);
+  $("#die2").on("click", handleClick);
 });
