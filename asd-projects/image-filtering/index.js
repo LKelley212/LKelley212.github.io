d405f991c1dca7eb5ffb771c324af6a55810ffd7
LKelley212 <laura.kelley@jpschools.org>
2025-11-14 22:31:21 +0000

    Image Todo 9 buggy

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,9 +20,8 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
-
+  // applyFilter(increaseGreenByBlue);
+  applyFilterNoBackground(reddify);
   // do not change the below line of code
   render($("#display"), image);
 }
@@ -32,18 +31,67 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
+function applyFilter(filterFunction) {
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      var pixel = image[i][j];
+      var pixelArray = rgbStringToArray(pixel);
+      // This is where I’ll modify the color values later
+      // pixelArray[RED] = 200;
+      filterFunction(pixelArray);
+      var updatedPixel = rgbArrayToString(pixelArray);
+      image[i][j] = updatedPixel;
 
+      /* console.log("pixel:", pixel);
+      // pixel looks like "rgb(150, 150, 150)"
+      console.log("pixelArray:", pixelArray);
+      // pixelArray looks like [150, 150, 150]
+      console.log("updatedPixel:", updatedPixel);
+      console.log("new stored value:", image[i][j]);
+      //verified that the image array has been updated to mach
+      */
+    }
+  }
+}
 
 // TODO 9 Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+  var backgroundColor = image[0][0];
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      var pixel = image[i][j];
+      // If the pixel array is not equal to the background color, apply the filter
+      if (pixel !== backgroundColor) {
+        var pixelArray = rgbStringToArray(pixel);
+        filterFunction(pixelArray);
+      }
 
-
+      var updatedPixel = rgbArrayToString(pixelArray);
+      image[i][j] = updatedPixel;
+    }
+  }
+}
 // TODO 6: Create the keepInBounds function
+function keepInBounds(num) {
+  return num < 0 ? (num = 0) : num > 255 ? 255 : num;
+}
 
+/* console.log(keepInBounds(-20)); // should print 0
+console.log(keepInBounds(300)); // should print 255
+console.log(keepInBounds(125)); // should print 125 */
 
 // TODO 4: Create reddify filter function
-
+function reddify(colorArray) {
+  colorArray[RED] = 200;
+}
 
 // TODO 7 & 8: Create more filter functions
-
-
+function decreaseBlue(colorArray) {
+  colorArray[BLUE] = colorArray[BLUE] - 50;
+  keepInBounds(colorArray[BLUE]);
+}
+function increaseGreenByBlue(colorArray) {
+  colorArray[GREEN] = colorArray[GREEN] + colorArray[BLUE];
+  keepInBounds(colorArray[GREEN]);
+}
 // CHALLENGE code goes below here
