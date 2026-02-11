53e1b6e6e20847b9ed01b2040cbb3bef82e80f6d
LKelley212 <laura.kelley@jpschools.org>
2026-02-11 17:25:33 +0000

    dice at 4

--- asd-projects/Blanks/dice-app/dice.js
+++ asd-projects/Blanks/dice-app/dice.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
   // Your code goes here
-
+  $("<div>")
+    .css("height", 75)
+    .css("width", 75)
+    .css("background-color", "#3457D5")
+    .css("position", "absolute")
+    .css("top", 200)
+    .css("left", 200)
+    .appendTo("#die");
 });
