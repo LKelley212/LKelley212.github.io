e83c6fcbd9ff44f3ff329efd788610eb1f5a46f0
LKelley212 <laura.kelley@jpschools.org>
2026-02-12 17:53:18 +0000

    Die Step 6 done

--- asd-projects/Blanks/dice-app/dice.js
+++ asd-projects/Blanks/dice-app/dice.js
@@ -7,6 +7,31 @@ $(document).ready(function () {
     .css("position", "absolute")
     .css("top", 212.5)
     .css("left", 212.5)
-    // .css("border-radius", "50%")
+    .css("border-radius", "50%")
     .appendTo("#die");
+
+  function makeDot(top, left, elementID) {
+    $("<div>")
+      .css("height", 75)
+      .css("width", 75)
+      .css("background-color", "#3457D5")
+      .css("position", "absolute")
+      .css("top", top)
+      .css("left", left)
+      .css("border-radius", "50%")
+      .appendTo(elementID);
+  }
+  makeDot(212.5, 212.5, "#die"); // middle middle
+  makeDot(87.5, 87.5, "#die"); // top left
+  makeDot(337.5, 337.5, "#die"); // bottom right
+
+  function rollDie(dieId) {
+    console.log("clicked");
+    var randomNum = Math.ceil(Math.random() * 6);
+    console.log(randomNum);
+  }
+  function handleClick() {
+    rollDie("#die");
+  }
+  $("#die").on("click", handleClick);
 });
