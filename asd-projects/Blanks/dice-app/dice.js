057a378b1f45040b2e263b6e79de00d52f9ca6f5
LKelley212 <laura.kelley@jpschools.org>
2026-02-26 17:35:35 +0000

    Dice app completed.

--- asd-projects/Blanks/dice-app/dice.js
+++ asd-projects/Blanks/dice-app/dice.js
@@ -21,14 +21,43 @@ $(document).ready(function () {
       .css("border-radius", "50%")
       .appendTo(elementID);
   }
-  makeDot(212.5, 212.5, "#die"); // middle middle
-  makeDot(87.5, 87.5, "#die"); // top left
-  makeDot(337.5, 337.5, "#die"); // bottom right
+  // makeDot(212.5, 212.5, "#die"); // middle middle
+  // makeDot(87.5, 87.5, "#die"); // top left
+  // makeDot(337.5, 337.5, "#die"); // bottom right
 
   function rollDie(dieId) {
+    $(dieId).empty();
     console.log("clicked");
     var randomNum = Math.ceil(Math.random() * 6);
     console.log(randomNum);
+    if (randomNum === 1) {
+      makeDot(212.5, 212.5, dieId); // middle middle
+    } else if (randomNum === 2) {
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(337.5, 337.5, dieId); // bottom right
+    } else if (randomNum === 3) {
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(212.5, 212.5, dieId); // middle middle
+    } else if (randomNum === 4) {
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // bottom left
+      makeDot(337.5, 87.5, dieId); // top right
+    } else if (randomNum === 5) {
+      makeDot(212.5, 212.5, dieId); // middle middle
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // top right
+      makeDot(337.5, 87.5, dieId); // bottom left
+    } else if (randomNum === 6) {
+      makeDot(337.5, 337.5, dieId); // bottom right
+      makeDot(87.5, 87.5, dieId); // top left
+      makeDot(87.5, 337.5, dieId); // top right
+      makeDot(337.5, 87.5, dieId); // bottom left
+      makeDot(212.55, 87.5, dieId); // middle left
+      makeDot(212.5, 337.5, dieId); // middle right
+    }
   }
   function handleClick() {
     rollDie("#die");
